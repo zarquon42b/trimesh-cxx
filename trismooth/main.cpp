fda7b55c5ffd1b5232b6996538aceaafe4997c2c
zarquon42b <schlager@anthro-hp-1.(none)>
2010-10-08 14:43:22 +0200

    added cmd tool to apply taubin smooth

---
+++ trismooth/main.cpp
@@ -0,0 +1,117 @@
+// Author: Andrea Tagliasacchi
+// Date: 22 May 2010
+#
+#include <string.h>
+#include <vector>
+using namespace std;
+
+// VCG headers for triangular mesh processing
+#include<vcg/simplex/edge/base.h>
+#include<vcg/simplex/vertex/base.h>
+#include<vcg/simplex/face/base.h>
+#include <vcg/complex/trimesh/base.h>
+#include <vcg/complex/trimesh/update/topology.h>
+#include <vcg/complex/trimesh/update/edges.h>
+#include <vcg/complex/trimesh/update/bounding.h>
+#include <vcg/complex/trimesh/update/quality.h>
+#include <vcg/complex/trimesh/update/color.h>
+#include <vcg/complex/trimesh/update/flag.h>
+#include <vcg/complex/trimesh/clean.h>
+#include <vcg/complex/intersection.h>
+#include <vcg/space/index/grid_static_ptr.h>
+#include <vcg/space/index/spatial_hashing.h>
+#include <vcg/complex/trimesh/closest.h>
+#include <vcg/complex/trimesh/smooth.h>
+// VCG File Format Importer/Exporter
+#include <wrap/io_trimesh/import.h>
+#include <wrap/io_trimesh/export.h>
+#include <wrap/io_trimesh/export_ply.h>
+
+using namespace vcg;
+
+class MyFace;
+class MyEdge;
+class MyVertex;
+struct MyUsedTypes : public UsedTypes<	Use<MyVertex>		::AsVertexType,
+Use<MyEdge>			::AsEdgeType,
+Use<MyFace>			::AsFaceType>{};
+class MyVertex  : public Vertex< MyUsedTypes, vertex::Coord3f, vertex::BitFlags, vertex::Normal3f, vertex::Mark,vertex::Color4b, vertex::Qualityf>{};
+class MyFace    : public Face  <MyUsedTypes, face::VertexRef,face::BitFlags,face::Mark, face::Normal3f> {};
+class MyMesh : public tri::TriMesh< vector<MyVertex>, vector<MyFace > >{};
+typedef MyMesh::ScalarType ScalarType;
+
+// Uncomment only one of the two following lines to test different data structures
+typedef vcg::GridStaticPtr<MyMesh::FaceType, MyMesh::ScalarType> TriMeshGrid;
+//typedef vcg::SpatialHashTable<MyMesh::FaceType, MyMesh::ScalarType> TriMeshGrid;
+
+int main(int argc,char ** argv){
+	char filename[256];
+  if (argc< 3){
+		printf("\n");
+    printf("    Convert a mesh to ascii ply format\n");
+    printf("    Usage: trimesh_project <input.mesh> <output.ply>\n");
+    printf("       <mesh>        any common mesh file (any common mesh file).\n");
+    printf("       <mesh>        mesh cleaned updated vertex normals saved in ascii PLY format.\n");
+    
+   
+		return 0;
+	}
+  /*else if (argc == 2)
+	{
+	strcpy(filename, argv[1]);
+	}	
+	else if (argc == 3)
+	{*/
+	strcpy(filename, argv[2]);
+	//}
+	
+	MyMesh mesh;
+  
+	
+		
+  //--------------------------------------------------------------------------------------//
+  //
+  //                                   OPENING THE FILES  
+  //
+  //--------------------------------------------------------------------------------------//
+
+  int err2 = tri::io::Importer<MyMesh>::Open(mesh,argv[1]);
+  if(err2) {
+		printf("Error in reading %s: '%s'\n",argv[1],tri::io::Importer<MyMesh>::ErrorMsg(err2));
+		exit(-1);  
+	}
+
+   //--------------------------------------------------------------------------------------//
+  //
+  //                                   PREPROCESS
+  //
+  // Update the bounding box and initialize max search distance
+  // Remove duplicates and update mesh properties
+  //--------------------------------------------------------------------------------------//
+
+  int dup = tri::Clean<MyMesh>::RemoveDuplicateVertex(mesh);
+        int unref =  tri::Clean<MyMesh>::RemoveUnreferencedVertex(mesh);
+  if (dup > 0 || unref > 0)
+                printf("Removed %i duplicate and %i unreferenced vertices from mesh %s\n",dup,unref,argv[2]);
+  tri::UpdateBounding<MyMesh>::Box(mesh);
+  tri::UpdateNormals<MyMesh>::PerFaceNormalized(mesh);
+  //tri::UpdateNormals<MyMesh>::PerVertexAngleWeighted(mesh);
+  tri::UpdateNormals<MyMesh>::NormalizeVertex(mesh);
+  tri::Smooth<MyMesh>::VertexCoordTaubin(mesh,10,0.5,-0.53);
+  tri::UpdateNormals<MyMesh>::PerFaceNormalized(mesh);
+  tri::UpdateNormals<MyMesh>::PerVertexAngleWeighted(mesh);
+  tri::UpdateNormals<MyMesh>::NormalizeVertex(mesh);
+
+
+
+  //--------------------------------------------------------------------------------------//
+ 
+
+  
+
+  tri::io::ExporterPLY<MyMesh>::Save(mesh,filename,tri::io::Mask::IOM_VERTNORMAL, false); // in ASCII
+
+
+
+  return 0;
+}
---
+++ trismooth/trismooth.pro
@@ -0,0 +1,9 @@
+TARGET = trismooth
+DEPENDPATH += . 
+INCLUDEPATH += . ../..
+CONFIG += console stl 
+TEMPLATE = app
+SOURCES += main.cpp ../../wrap/ply/plylib.cpp
+
+# Mac specific Config required to avoid to make application bundles
+CONFIG -= app_bundle
