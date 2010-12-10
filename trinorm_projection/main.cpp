cbac71e883950f7f338964e9edcf1dc5b5101990
schlager <schlagerlogistik@online.de>
2010-12-10 22:53:32 +0100

    integrated triray_project into trinorm_project

--- trinorm_projection/main.cpp
+++ trinorm_projection/main.cpp
@@ -45,20 +45,66 @@ typedef vcg::GridStaticPtr<MyMesh::FaceType, MyMesh::ScalarType> TriMeshGrid;
 
 int main(int argc,char ** argv){
      char filename[256];
+     bool bcheck = false;
+     bool scheck = false;
+     bool noout = false;
+     bool tolcheck = false;
+     float tol = 1e3;
+     float tol1;
   if (argc<3){
 		printf("\n");
     printf("    Compute a projection of a point cloud onto a mesh along given normals\n if No face is hit, the closest point on the target mesh is used");
     printf("    Usage: trinorm_project <cloud> <mesh>\n");
     printf("       <cloud>        Point cloud containing starting points, including normal information of vertices (PLY format).\n");
+    printf("       [-b | --tolout | --tolout ]        -b = check both directions of normal.\n");
+    printf("                                          --tolout = max distance to look in normal direction.\n");
+    printf("                                          --tolin = max distance to look in opposite direction of the normal.\n");
     printf("       <mesh>         Mesh model over which to project (PLY format).\n");
     printf("\n");
-    printf("Saves the projected point cloud in the file out_cloud.ply when terminated.\n");
+    printf("Saves the projected point cloud in the file out_norm.ply when terminated.\n");
     printf("The vertex coordinates represent the projected samples (in same order as input.\n");
     printf("The vertex quality (requires ply files) represents the projection distance.\n");
 		return 0;
 	}
+    for (int i = 1; i < argc; i++) {
+  if (i + 1 != argc) {// Check that we haven't finished parsing already
 
-    strcpy(filename, argv[2]);
+
+
+                        if (strcmp("--tolout", argv[i]) == 0)
+                        {tol = atof(argv[i+1]);
+                            if (i==argc-2)
+                            {noout=true;}
+                        }
+
+                        if (strcmp("--tolin", argv[i]) == 0)
+                        {tol = atof(argv[i+1]);
+                        tolcheck = true;
+                        if (i==argc-2)
+                            {noout=true;}
+                        }
+                    }
+            }
+
+    for (int i = 1; i < argc; i++) {
+            if (strcmp("-b", argv[i]) == 0)
+            {bcheck=true;
+                if (i==argc-1)
+                {noout=true;}
+            }
+
+        }
+
+
+
+    if (noout==true)
+           {printf("Error: please specify target mesh\n");
+               return 0;
+           }
+    if (tolcheck == false)
+        {tol1=tol;
+        }
+    strcpy(filename, argv[1]);
     MyMesh mesh;
     MyMesh in_cloud;
     MyMesh out_cloud;
@@ -73,9 +119,9 @@ int main(int argc,char ** argv){
 		printf("Error in reading %s: '%s'\n",argv[1],tri::io::Importer<MyMesh>::ErrorMsg(err));
 		exit(-1);  
 	}
-  int err2 = tri::io::Importer<MyMesh>::Open(mesh,argv[2]);
+  int err2 = tri::io::Importer<MyMesh>::Open(mesh,argv[argc-1]);
   if(err2) {
-		printf("Error in reading %s: '%s'\n",argv[2],tri::io::Importer<MyMesh>::ErrorMsg(err2));
+                printf("Error in reading %s: '%s'\n",argv[argc-1],tri::io::Importer<MyMesh>::ErrorMsg(err2));
 		exit(-1);  
 	}
   // Allocate space for projected cloud
@@ -99,6 +145,7 @@ int main(int argc,char ** argv){
   tri::UpdateNormals<MyMesh>::PerFaceNormalized(mesh);
   tri::UpdateNormals<MyMesh>::PerVertexAngleWeighted(mesh);
   tri::UpdateNormals<MyMesh>::NormalizeVertex(mesh);
+
   //tri::UpdateBounding<MyMesh>::Box(mesh);
   tri::UpdateNormals<MyMesh>::NormalizeVertex(in_cloud);
   tri::UpdateQuality<MyMesh>::VertexConstant(out_cloud, 0);
@@ -138,7 +185,8 @@ int main(int argc,char ** argv){
     ray.SetOrigin(orig);
     ray.SetDirection(dir);
 
-
+    if (bcheck == false)
+    {
     MyFace* f_ptr=GridDoRay(static_grid,FintFunct, mf, ray, maxDist, t);
 
         if (f_ptr)
@@ -165,6 +213,59 @@ int main(int argc,char ** argv){
             out_cloud.vert[i].Q() = minDist;
         }
 
+  }
+
+  else
+  {MyFace* f_ptr=GridDoRay(static_grid,FintFunct, mf, ray, maxDist, t);
+
+      if (f_ptr && t < tol) //check if face is hit and if distance is below threshold
+
+      {   MyMesh::CoordType tt = in_cloud.vert[i].P()+in_cloud.vert[i].N()*t;
+          int f_i = vcg::tri::Index(mesh, f_ptr);
+          MyMesh::CoordType ti = (mesh.face[f_i].V(0)->N()+mesh.face[f_i].V(1)->N()+mesh.face[f_i].V(2)->N())/3;
+          double t0;
+          t0 = sqrt(ti[0]*ti[0]+ti[1]*ti[1]+ti[2]*ti[2]);
+          out_cloud.vert[i].N() = ti/t0;
+          out_cloud.vert[i].Q() = t;
+          out_cloud.vert[i].P() = tt;
+
+      }
+      else
+      {
+          MyFace* f_ptr=GridDoRay(static_grid,FintFunct, mf, ray, maxDist, t);
+          ray.SetDirection(-dir);         // search the other direction of the normal
+          if (f_ptr && t < tol1)  //check if face is hit and if distance is below threshold
+
+              {MyMesh::CoordType tt = in_cloud.vert[i].P()+in_cloud.vert[i].N()*t;
+              int f_i = vcg::tri::Index(mesh, f_ptr);
+              MyMesh::CoordType ti = (mesh.face[f_i].V(0)->N()+mesh.face[f_i].V(1)->N()+mesh.face[f_i].V(2)->N())/3;
+              double t0;
+              t0 = sqrt(ti[0]*ti[0]+ti[1]*ti[1]+ti[2]*ti[2]);
+              out_cloud.vert[i].N() = ti/t0;
+              out_cloud.vert[i].Q() = t;
+              out_cloud.vert[i].P() = tt;
+
+          }
+
+
+
+
+          else //find the closest point if theres nothing along the ray
+
+          {  // printf("Couldn't trace landmark %d along ray on %s: closest point on target will be sought\n",i+1,target);
+          Point3f& currp = in_cloud.vert[i].P();
+          Point3f& clost = out_cloud.vert[i].P();
+          MyFace* f_ptr=GridClosest(static_grid, PDistFunct, mf, currp, maxDist, minDist, clost);
+          int f_i = vcg::tri::Index(mesh, f_ptr);
+          MyMesh::CoordType ti = (mesh.face[f_i].V(0)->N()+mesh.face[f_i].V(1)->N()+mesh.face[f_i].V(2)->N())/3;
+          double t0;
+          t0 = sqrt(ti[0]*ti[0]+ti[1]*ti[1]+ti[2]*ti[2]);
+          out_cloud.vert[i].N() = ti/t0;
+          out_cloud.vert[i].Q() = minDist;
+          }
+      }
+
+    }
   }
 
   int t2 = clock();
